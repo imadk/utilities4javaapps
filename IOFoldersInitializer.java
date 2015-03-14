5000ecc5b9e4126a3e39be40a7a086b5360b26f0
imadk <ielkhoury@gmail.com>
2015-03-14 16:40:21 -0400

    Update IOFoldersInitializer.java

--- IOFoldersInitializer.java
+++ IOFoldersInitializer.java
@@ -14,58 +14,40 @@ import java.io.File;
 import java.io.IOException;
 
 public class IOFoldersInitializer {
-
-
 	public static boolean createFolders() throws IOException {
-
-
 		// Create dinternal folder
 		File fol_DInternal = new File(
 				IOWires.getDInternalPath(IOWires.exportedProduct));
-
 		// Create output folder
 		File fol_Out = new File(IOWires.getPath(IOWires.exportedProduct,
 				"OutputFiles"));
-
 		try {
 			fol_DInternal.mkdir();
 			fol_Out.mkdir();
 		} catch (Exception e) {
-
 			//"An error occurred while trying to set up the I/O environment");
 		}
-
-
 		return true;
 	}
 
 	public static void deleteOutputFolder() throws IOException {
-
 		// Delete output folder
 		File fol_Out = new File(IOWires.getPath(IOWires.exportedProduct,
 				"OutputFiles"));
-
 		if (fol_Out.exists()) {
-
 			try {
-
 				if (deleteDir(fol_Out)) {
-
 					// recreate empty folder
 					File fol_Out2 = new File(IOWires.getPath(
 							IOWires.exportedProduct, "OutputFiles"));
 					fol_Out2.mkdir();
-
 				} else {
-
 					//"An error occurred while trying to delete the internal out folder.");
 				}
-
 			} catch (Exception e) {
 					//"An error occurred while trying to delete the internal out folder.");
 			}
 		}
-
 	}
 
 	// Deletes all folder's files and subdirectories.
@@ -79,8 +61,6 @@ public class IOFoldersInitializer {
 				}
 			}
 		}
-
 		return dir.delete();
 	}
-
 }
